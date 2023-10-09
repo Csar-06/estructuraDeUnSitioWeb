e43be84899c23f4ec1c4b78cfdb128cbaa662ef7
csarj <csarjdeveloper@gmail.com>
2023-10-09 14:30:38 -0600

    practica navegacion

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const { log } = require('console');
 const express = require('express');
 const app = express()
 const port = 8000
@@ -9,8 +8,22 @@ app.use(express.static(path.resolve(__dirname, '../public')))
 
 console.log(__dirname,'../public');
 
+app.listen(port, ()=> console.log('Corriendo en el puerto 8000'))
+
+
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname ,'./views/home.html'))
 })
 
-app.listen(port, ()=> console.log('Corriendo en el puerto 8000'))
+app.get('/Ofertas', (req, res)=>{
+    res.sendFile(path.resolve(__dirname ,'./views/ofertas.html'))
+})
+
+app.get('/tiendasOficiales', (req, res)=>{
+    res.sendFile(path.resolve(__dirname ,'./views/tiendasOficiales.html'))
+})
+
+app.get('/*', (req, res)=>{
+    res.send('Not Found')
+})
+
